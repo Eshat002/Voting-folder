8bda0961f1e422c92c61a72c4ce616d0fc2129f6
hp <nolanvenus33@gmail.com>
2022-05-17 19:50:41 +0600

    'ds'

--- whois/settings.py
+++ whois/settings.py
@@ -135,6 +135,8 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
  
