8eedf5f8131fde9a4f23d52b74a6d4ca1da4f6f0
hp <nolanvenus33@gmail.com>
2022-06-26 19:09:35 +0600

    d

--- whois/settings.py
+++ whois/settings.py
@@ -209,7 +209,8 @@ AWS_QUERYSTRING_AUTH=config("AWS_QUERYSTRING_AUTH",cast=bool)
 
 LOGIN_URL = '/accounts/login/'
 
-SECURE_SSL_REQUIRED=True
+SECURE_SSL_REDIRECT = True
+
 
 # if os.getcwd() == "/app":
 #     SECURE_PROXY_SSL_HEADER=('HTTP_X_FORWARDED_PROTO','https')
