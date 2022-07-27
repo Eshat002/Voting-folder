a038c04525744a8da2e4114e902b48490d538d4d
hp <nolanvenus33@gmail.com>
2022-07-27 12:14:45 +0600

    d

--- whois/settings.py
+++ whois/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1","www.comparestupids.com"]
+ALLOWED_HOSTS = ["127.0.0.1","www.comparestupids.com","mangofever.herokuapp.com"]
 
 
 # Application definition
